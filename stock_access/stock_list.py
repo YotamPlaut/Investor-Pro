c60b26ba7dff1c03723dcc537498cc6e38b23de2
IshayBalash <ishay6411@gmail.com>
2024-05-18 13:06:50 +0300

    adding server database interaction function for server external use

--- stock_access/stock_list.py
+++
@@ -1,8 +0,0 @@
-stock_list = [
-    {'index': 142, 'name': 'tel_aviv_35'},
-    {'index': 143, 'name': 'tel_aviv_90'},
-    {'index': 147, 'name': 'semi_60'},
-    {'index': 148, 'name': 'finaces'},
-    {'index': 169, 'name': 'top_100_tech'},
-    {'index': 601, 'name': 'all_bonds'}
-]
