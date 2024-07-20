8c6c0404af8d9370c94087ecae01ad938b3a9e5c
IshayBalash <ishay6411@gmail.com>
2024-07-20 13:49:47 +0300

    function for server use

--- GCP_SETUP/server_use_functions.py
+++ GCP_SETUP/server_use_functions.py
@@ -125,7 +125,7 @@ def get_all_stocks():
         print(f"error occurred while running query: {e}")
 
 
-def get_last_update_stock_stats(stock_name: str, stats_name : str):
+def get_last_update_stock_stats_by_stats_name(stock_name: str, stats_name : str):
     matching_stock_index = next(
         (stock['index_id'] for stock in stock_list if stock['name'] == stock_name),
         None)
@@ -164,6 +164,56 @@ def get_last_update_stock_stats(stock_name: str, stats_name : str):
         return None
 
 
+def get_all_last_update_stock_stats(stock_name: str):
+    matching_stock_index = next(
+        (stock['index_id'] for stock in stock_list if stock['name'] == stock_name),
+        None)
+    if matching_stock_index is None:
+        print(f"didn't found matching index for stock: {stock_name}")
+        return None
+    if matching_stock_index is None:
+        print(f"didn't found matching index for stock: {stock_name}")
+        return None
+    try:
+        engine = get_pool()
+        query = f"""
+        with max_dates as(
+                        select 
+                            stats_name,
+                            max(insert_time) as max_insert_time
+                            from  {table_configs['stocks']['stats']}
+                            where index_symbol={matching_stock_index}
+                            group by 1
+                        )
+            select 
+                a.stats_name,
+                a.stats_info,
+                a.insert_time,
+                a.index_symbol,
+                a.symbol_name
+            from  {table_configs['stocks']['stats']} a inner join max_dates b 
+            on a.stats_name=b.stats_name
+            where 
+                a.index_symbol={matching_stock_index}
+                and b.max_insert_time=a.insert_time;
+               """
+        with engine.connect() as conn:
+            result = conn.execute(text(query)).fetchall()
+            stock_stats_dict = {}
+            for row in result:
+                # date_str = row['date'].strftime('%Y-%m-%d')  # Ensure date is in string format for JSON compatibility
+                stock_stats_dict[row[0]] = {
+                    'stats_info': row[1],
+                    'insert_time': row[2].strftime('%Y-%m-%d')
+                }
+            stock_stats_dict['Index_Symbol'] = result[0][3]
+            stock_stats_dict['symbol_name'] = result[0][4]
+            stock_stats_dict = json.dumps(stock_stats_dict)
+            return stock_stats_dict
+
+    except Exception as e:
+        print(f"error occurred while running query: {e}")
+        return None
 
 
 ##server functions
@@ -416,7 +466,8 @@ if __name__ == '__main__':
     #
     # get stock data by day example
     #print(get_stock_data_by_date('Bank_Discont', '2024-05-06'))
-    print(get_last_update_stock_stats('Bank_Discont', 'sharpe_ratio'))
+    #print(get_last_update_stock_stats_by_stats_name('Bank_Discont', 'sharpe_ratio'))
+    print(get_all_last_update_stock_stats('Bank_Discont'))
 
 
     # print(get_all_portfolios(user_id='shahar_tst'))
