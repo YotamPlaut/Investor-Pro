116c7673c5693bf1f319e8ab9b127e5d25c54a44
IshayBalash <ishay6411@gmail.com>
2024-09-09 20:56:03 +0300

    ddd

--- backend/classes_backend/stock.py
+++
@@ -1,4 +0,0 @@
-class Stock:
-    def __init__(self, name: str, number: int):
-        self.stock_num = number
-        self.stock_name = name
--- backend/classes_backend/user.py
+++
@@ -1,5 +0,0 @@
-class User():
-    def __init__(self, username: str, email: str, password: str):
-        self.username = username
-        self.email_address = email
-        self.password = password
